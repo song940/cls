8846686ab1a43a2a51dc3e2cb29afb55d0af0338
Lsong <song940@gmail.com>
2020-04-27 18:02:21 +0800

    fix const

--- index.js
+++ index.js
@@ -3,17 +3,21 @@ function cls() {
   return []
     .slice
     .call(arguments)
-    .map((c) => {
+    .map(function (c) {
       switch (/\[object (\w+)\]/.exec(({}).toString.call(c))[1]) {
         case 'Array': return c;
         case 'String': return [c];
-        case 'Object': return Object.keys(c).filter((k) => c[k]);
+        case 'Object': return Object.keys(c).filter(function (k) {
+          return c[k];
+        });
       }
       return [];
     })
-    .reduce((p, c) => p.concat(c), [])
-    .filter((c) => !!c)
-    .reduce((p, c) => {
+    .reduce(function (p, c) {
+      return p.concat(c)
+    }, [])
+    .filter(Boolean)
+    .reduce(function (p, c) {
       if (p.indexOf(c) < 0) p.push(c);
       return p;
     }, []).join(' ');
