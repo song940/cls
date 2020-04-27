ac582e9e4737acddfc7f9485ee95f61efa00b988
Lsong <song940@gmail.com>
2020-04-27 17:13:35 +0800

    use function instaed const

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 
-const cls = (...cls) => {
-  return cls.map((c) => {
-    switch(/\[object (\w+)\]/.exec(({}).toString.call(c))[1]){
-      case 'Array' : return   c;
-      case 'String': return [ c ];
-      case 'Object': return Object.keys(c).filter((k) => c[k]);
-    }
-    return [];
-  })
-  .reduce((p, c) => p.concat(c), [])
-  .filter((c) => !!c)
-  .reduce((p, c) => {
-    if(p.indexOf(c) < 0) p.push(c);
-    return p;
-  }, []).join(' ');
+function cls() {
+  return []
+    .slice
+    .call(arguments)
+    .map((c) => {
+      switch (/\[object (\w+)\]/.exec(({}).toString.call(c))[1]) {
+        case 'Array': return c;
+        case 'String': return [c];
+        case 'Object': return Object.keys(c).filter((k) => c[k]);
+      }
+      return [];
+    })
+    .reduce((p, c) => p.concat(c), [])
+    .filter((c) => !!c)
+    .reduce((p, c) => {
+      if (p.indexOf(c) < 0) p.push(c);
+      return p;
+    }, []).join(' ');
 }
 
 module.exports = cls;
---
+++ test/index.js
@@ -0,0 +1,3 @@
+const cls = require('..');
+
+console.log(cls('a', 'b', { c: 1 }));
